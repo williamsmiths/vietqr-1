9394602d56275a0b74161dad4b1b3f6429838690
Kong <conghuancse@gmail.com>
2025-10-07 10:59:48 +0700

    Remove go-qrcode dependency from demo

--- demo/main.go
+++ demo/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"image/png"
-	"os"
-
-	"github.com/skip2/go-qrcode"
 	"github.com/sunary/vietqr"
 )
 
@@ -17,29 +13,6 @@ func main() {
 	})
 
 	println("VietQR String:", qrString)
-
-	// Tạo mã QR code hình ảnh
-	qr, err := qrcode.New(qrString, qrcode.Medium)
-	if err != nil {
-		println("Lỗi tạo QR:", err.Error())
-		return
-	}
-
-	// Lưu file QR code
-	file, err := os.Create("vietqr.png")
-	if err != nil {
-		println("Lỗi tạo file:", err.Error())
-		return
-	}
-	defer file.Close()
-
-	err = png.Encode(file, qr.Image(256))
-	if err != nil {
-		println("Lỗi lưu QR:", err.Error())
-		return
-	}
-
-	println("Đã tạo file vietqr.png thành công!")
 
 	// Test decode
 	info, err := vietqr.Decode(qrString)
